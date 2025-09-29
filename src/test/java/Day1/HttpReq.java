1fbe44486510e609c7b00b78ecfb158e545db50a
tharaniramaswamy-wq <tharaniramaswamy@gmail.com>
2025-09-29 22:11:45 +0530

    commit

--- src/test/java/Day1/HttpReq.java
+++ src/test/java/Day1/HttpReq.java
@@ -19,23 +19,23 @@ public class HttpReq {
 		.log().all();
 	
 }
-@Test
-
-void createUser() {
-	HashMap hm = new HashMap();
-	hm.put("id", "0");
-	hm.put("idBook","0");
-	hm.put("firstName", "thara");
-	hm.put("lastName", "rmswmy");
-	given()
-	.contentType("application/json")
-	.body(hm)
-	.when()
-	.post("https://fakerestapi.azurewebsites.net/api/v1/Authors")
-	.then()
-	.statusCode(200)
-	.log().all();
-}
+//@Test
+//
+//void createUser() {
+//	HashMap hm = new HashMap();
+//	hm.put("id", "0");
+//	hm.put("idBook","0");
+//	hm.put("firstName", "thara");
+//	hm.put("lastName", "rmswmy");
+//	given()
+//	.contentType("application/json")
+//	.body(hm)
+//	.when()
+//	.post("https://fakerestapi.azurewebsites.net/api/v1/Authors")
+//	.then()
+//	.statusCode(200)
+//	.log().all();
+//}
 
 
 }
